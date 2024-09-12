20ae263e26370d7ab8e94393a8ab1bf1f177e40e
ErwinWasko <e.skowron@wasko.pl>
2024-09-12 12:00:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import skaner
 import time
 
@@ -16,18 +16,30 @@ def index():
 def fetch_reports():
     global current_reports, all_reports_fetched
 
-    if all_reports_fetched:
+    if all_reports_fetched and not current_reports:
         return jsonify({})  # Zwraca pustą odpowiedź, jeśli wszystkie raporty zostały już przetworzone
 
     if not current_reports:
         current_reports = skaner.fetch_security_reports()
-    
-    if current_reports:
+
+    selected_cvss = request.args.get('cvss', 'all')
+
+    while current_reports:
         report = current_reports.pop(0)
         cve = report.get('vulnerability')
-        cvss = report.get('vulnerability_score', 'N/A')
+        cvss = float(report.get('vulnerability_score', 0))
         description = report.get('vulnerability_description', 'No description available')
         
+        if selected_cvss != 'all':
+            if selected_cvss == 'low' and not (0.1 <= cvss <= 3.9):
+                continue
+            if selected_cvss == 'medium' and not (4.0 <= cvss <= 6.9):
+                continue
+            if selected_cvss == 'high' and not (7.0 <= cvss <= 8.9):
+                continue
+            if selected_cvss == 'critical' and not (9.0 <= cvss <= 10.0):
+                continue
+
         mitre_data = skaner.fetch_mitre_data(cve)
         description, ref_urls = skaner.process_mitre_data(mitre_data)
         
@@ -47,8 +59,8 @@ def fetch_reports():
         time.sleep(2)  # Symulowanie przetwarzania danych
         
         return jsonify(result)
-    else:
-        return jsonify({})
+    
+    return jsonify({})
 
 if __name__ == '__main__':
     app.run(debug=True)
